1240072906008536ad28f9298de772e5471fbc3d
anaj-krishna <anajkrishna36@gmail.com>
2026-01-12 12:09:19 +0530

    chor(): created empty files

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,15 @@ import { setupApp } from './config/app-bootstrap';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*',
+    credentials: true,
+  });
+
   setupApp(app);
-  await app.listen(process.env.PORT || 3000);
+
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
+
 bootstrap().catch(console.error);
