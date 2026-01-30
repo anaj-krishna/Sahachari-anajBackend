ab396686cca7ee9aa423685a731bb82a88311fce
Vinayak S <vinayaksukhalal@gmail.com>
2026-01-30 14:08:57 +0530

    updated

--- dist/auth/dto/register.dto.d.ts
+++ dist/auth/dto/register.dto.d.ts
@@ -2,7 +2,7 @@ import { Role } from '../../common/enums/role.enum';
 export declare class RegisterDto {
     name: string;
     email: string;
-    shopAddress: string;
+    address: string;
     serviceablePincodes: string[];
     password: string;
     role: Role;
